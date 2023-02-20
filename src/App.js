488abfd9d35a31d50f895aa82492b6917edbc226
techNiru <nirali.aprilinnovations@gmail.com>
2023-02-20 18:04:33 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+  // button
+  const listOfItem = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
+  // delete items onclick(todolist)
+  const deleteItems = (id) => {
+    console.log("deleted");
 
-function App() {
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List </h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add a Items"
+            value={inputList}
+            onChange={itemEvent}
+          />
+          <button onClick={listOfItem}> + </button>
+          <ol>
+            {/* <li>{inputList}</li> */}
+            {/* using map for items (value & key value and id ) */}
+
+            {Items.map((itemVal, index) => {
+              return (
+                <ToDoList
+                  key={index}
+                  id={index}
+                  text={itemVal}
+                  onSelect={deleteItems}
+                />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+    return (
+    <>
+    <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List </h1>  
+          <br/>
+          <input type="text" placeholder="Add a Items" />
+        <button> + </button>
+        <ol>
+            <li>Buy a apple</li>
+        </ol>
+        </div>
+    </div>
+        </>
+    )
+};
+
+export default App;
